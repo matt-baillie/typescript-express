ceca6e87047d4597bcd3fe266c7736321152ecf6
Matthew Baillie <mbaillie93@gmail.com>
2022-10-14 11:01:46 -0700

    project complete

---
+++ dist/app.js
@@ -0,0 +1,16 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const PORT = 4000;
+const app = (0, express_1.default)();
+const add = (a, b) => {
+    return a + b;
+};
+app.get("/", (req, res, next) => {
+    console.log(add(5, 5));
+    res.send("llo");
+});
+app.listen(PORT, () => console.log("Server running"));
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
-import express from "express";
+import express, { Application, Request, Response, NextFunction } from "express";
 const PORT = 4000;
 
-const app = express();
+const app: Application = express();
 
-app.get("/", (req, res) => {
-  res.send("hello");
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  console.log(add(5, 5));
+
+  res.send("llo");
 });
 
 app.listen(PORT, () => console.log("Server running"));
